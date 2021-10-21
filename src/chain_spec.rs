8e2a64359f75d451a081dfa38e096856629bd2ad
mustermeiszer <mustermeiszer@posteo.de>
2021-10-21 15:40:22 +0200

    Remove changes to issuance

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -260,7 +260,7 @@ pub fn altair_dev(para_id: ParaId) -> AltairChainSpec {
 					// 	.unchecked_into(),
 				)],
 				endowed_accounts(),
-				Some(10_000_000_000 * CFG),
+				None,
 				para_id,
 			)
 		},
